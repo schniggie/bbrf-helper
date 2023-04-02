9fded28aa35f7cf28355f2d386220d7b565953a7
ᄃΉЯIƧƬӨPΉΣЯ (BᄂΣᄃKMΛПП-)DЯΣΉΣЯ <root@schniggie.de>
2023-04-02 20:41:43 +0200

    Update afuzz.sh

--- urls/afuzz.sh
+++ urls/afuzz.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Fuzz URLs with afuzz
+# Fuzz URLs with afuzz and add all with status 200
 # Requirments: afuzz, jq
 
+
 while read -r line; do afuzz -u $line; done < <(bbrf urls -r);
+cat result/* | jq -r '.result[] | select ( .status | contains(200) ) | .url + " " + (.status|tostring) + " " + (.length[]|tostring)' | bbrf url add - -p @INFER --show-new
