a4df50c71478dc90c1ebf98f810b4d5a631dab04
ᄃΉЯIƧƬӨPΉΣЯ (BᄂΣᄃKMΛПП-)DЯΣΉΣЯ <root@schniggie.de>
2023-04-18 17:17:02 +0200

    Update rad.sh

--- urls/rad.sh
+++ urls/rad.sh
@@ -11,4 +11,4 @@ done < <(bbrf urls -r);
 cat rad/*.json | jq -r '.[] | select ( .Method | contains("GET") ) | .URL' | uro | grep -Eiv ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg)" | httpx -include-response -json -silent | tee >(httpx2bbrf -s rad --show-new)
 # Import all POST Requests
 cat rad/*.json | jq -r '.[] | select ( .Method | contains("POST") ) | .URL +","+ (.b64_body | @base64d)' | tee rad/post.csv
-while IFS="," read -r url postbody; do echo "Testing POST URL: $url"; httpx -include-response -json -silent -u '$url' -x POST -body '$postbody' | httpx2bbrf -s rad -t postbody:'$postbody' --show-new; done < <(cat rad/post.csv)
+while IFS="," read -r url postbody; do echo "Testing POST URL: $url"; httpx -include-response -json -silent -u '$url' -x POST -body '$postbody' | httpx2bbrf -s rad -t method:POST -t postbody:'$postbody' --show-new; done < <(cat rad/post.csv)
