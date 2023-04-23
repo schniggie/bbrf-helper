87c60437a8dcd118c40099952ed295cf0b5daafd
ᄃΉЯIƧƬӨPΉΣЯ (BᄂΣᄃKMΛПП-)DЯΣΉΣЯ <root@schniggie.de>
2023-04-23 17:07:45 +0200

    Update rad.sh

--- urls/rad.sh
+++ urls/rad.sh
@@ -8,7 +8,7 @@ while read -r line; do
 done < <(bbrf urls -r);
 
 # Import all GET Requests
-cat rad/*.json | jq -r '.[] | select ( .Method | contains("GET") ) | .URL' | uro | grep -Eiv ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg)" | httpx -include-response -json -silent | tee >(httpx2bbrf -s rad --show-new)
+cat rad/*.json | jq -r '.[] | select ( .Method | contains("GET") ) | .URL' | uro | grep -Eiv ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg)" | httpx -include-response -json -silent | tee >(httpx2bbrf -s rad -t method:GET --show-new)
 # Import all POST Requests
 cat rad/*.json | jq -r '.[] | select ( .Method | contains("POST") ) | .URL +" "+ (.b64_body | @base64d)' | tee rad/post.req
-read -r url postbody; do httpx -include-response -json -silent -u $url -x POST -body $postbody | httpx2bbrf -s rad -t method:POST -t postbody:$postbody --show-new; done < cat rad/post.req
+read -r url postbody; do httpx -include-response -json -silent -u $url -x POST -body $postbody | httpx2bbrf -s rad -t method:POST -t postbody:$postbody --append-tags --show-new; done < cat rad/post.req
