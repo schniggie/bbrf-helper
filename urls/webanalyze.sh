aa7f5bafa33c28bc28ff0a0aa19738e0b25cc4da
ᄃΉЯIƧƬӨPΉΣЯ (BᄂΣᄃKMΛПП-)DЯΣΉΣЯ <root@schniggie.de>
2023-04-24 22:56:40 +0200

    Update webanalyze.sh

--- urls/webanalyze.sh
+++ urls/webanalyze.sh
@@ -4,11 +4,9 @@
 # Requirements: webanalyze, httpx, https://github.com/schniggie/httpx2bbrf
 
 while read -r line; do
-webanalyze -host $line  -silent -output json | jq -r '.matches[] | .app_name + " " + .version';
+webanalyze -host $line  -silent -output json | tee webanalyze/${line##h*/}.json;
 done < <(bbrf urls -r);
 
-# Import all GET Requests
-cat rad/*.json | jq -r '.[] | select ( .Method | contains("GET") ) | .URL' | uro | grep -Eiv ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg)" | httpx -include-response -json -silent | tee >(httpx2bbrf -s rad --show-new)
-# Import all POST Requests
-cat rad/*.json | jq -r '.[] | select ( .Method | contains("POST") ) | .URL +" "+ (.b64_body | @base64d)' | tee rad/post.req
-read -r url postbody; do httpx -include-response -json -silent -u $url -x POST -body $postbody | httpx2bbrf -s rad -t method:POST -t postbody:$postbody --show-new; done < cat rad/post.req
+# Import tags and update 
+cat webanalyze/*.json  | jq -r '.hostname as $hostname | .matches[] | [.app.category_names[0], .app_name, .version] | select(length > 0) | [$hostname] + . | map( @sh ) | join(" ")' | tee webanalyze/webanalyze.tags
+read -r url cat app version; do httpx -include-response -json -silent -u $url | httpx2bbrf -t $cat:$app.$version --append-tags; done < cat webanalyze/webanalyze.tags
